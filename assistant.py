5435789f9ab89e064012d41fbf3b2c143a8c297c
AryaSriva <87484641+AryaSriva@users.noreply.github.com>
2025-02-12 19:42:39 -0500

    added makeshift RAG(just store everything in a text file lol)

--- assistant.py
+++ assistant.py
@@ -1,16 +1,50 @@
-from openai import OpenAI
+import openai
 import os
 from dotenv import load_dotenv
+import signal
+
 
 load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
-completion = client.chat.completions.create(
-    model="gpt-4",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Write a haiku about recursion in programming."},
-    ],
+token = os.getenv("GITHUB_TOKEN")
+endpoint = "https://models.inference.ai.azure.com"
+model_name = "gpt-4o-mini"
+client = openai.OpenAI(
+    base_url=endpoint,
+    api_key=token,
 )
+messages = []
+with open("context.txt", "r+") as file:
+    for line in file:
+        if line != "":
+            role = line.split(":")[0]
+            content = line.split(":")[1][:-1]
+            messages += [{"role": role, "content": content}]
+if messages == []:
+    messages += [{"role": "system", "content": "You are a helpful assistant."}]
 
-print(completion.choices[0].message)
+
+def signal_handler(messages):
+    print("Okay see ya!")
+    context = ""
+    for message in messages:
+        context += f"{message[role]}:{message[content]}\n"
+    file.write(context)
+    file.close()
+    exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+print("Assistant: Hello! What's on your mind?")
+while True:
+    user_message = input("User: ")
+    messages += [{"role": "user", "content": user_message}]
+    response = client.chat.completions.create(
+        messages=messages,
+        temperature=1.0,
+        top_p=1.0,
+        max_tokens=1000,
+        model=model_name,
+    )
+    print(f"Assistant: {response.choices[0].message.content}")
+    messages += [response.choices[0].message]
